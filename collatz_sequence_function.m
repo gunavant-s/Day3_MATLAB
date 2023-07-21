7132af19b6181e74b78c61fbff805c668a3f71b9
Gunavant <139606020+gunavant-s@users.noreply.github.com>
2023-07-22 05:21:07 +0530

    Create collatz_sequence_function.m

---
+++ collatz_sequence_function.m
@@ -0,0 +1,16 @@
+function vseq = collatz(n)
+    i = 1;
+    vseq(i) = n;
+    while n > 1 
+        if mod(n,2) == 0
+            i = i + 1;
+            n = n/2 ;
+            vseq(i) = n;
+        else
+            i = i + 1;
+            n = 3*n + 1;
+            vseq(i) = n;
+        end
+    end
+end
+
